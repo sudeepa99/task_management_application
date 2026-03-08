d93b0fa1c79327ee9888f421fed8221ae591f5ba
sudeepa99 <sudeepa1234nisal@gmail.com>
2026-03-08 17:16:09 +0530

    user entity defined

---
+++ Backend/task-management/task-management/src/main/java/com/taskmanagement/task_management/entity/User.java
@@ -0,0 +1,26 @@
+package com.taskmanagement.task_management.entity;
+
+import com.taskmanagement.task_management.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
