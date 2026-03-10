f819d5d102e0eb29555fee9627ab492736c2cc83
sudeepa99 <sudeepa1234nisal@gmail.com>
2026-03-11 00:31:27 +0530

    defined layout.jsx with toaster notification also

---
+++ Frontend/task-management/src/app/layout.jsx
@@ -0,0 +1,17 @@
+// src/app/layout.jsx
+import { AuthProvider } from "@/context/AuthContext";
+import "./globals.css";
+import { Toaster } from "react-hot-toast";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <AuthProvider>
+          {children}
+          <Toaster position="top-right" />
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
